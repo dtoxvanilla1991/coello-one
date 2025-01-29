606ff09ffc96d1a0561deb584e18f355e84d3b80
dtoxvanilla1991 <dtoxvanilla@gmail.com>
2025-01-28 21:41:31 -0500

    chores

--- coello-one/app/components/navbar-components/NavbarSearch/index.tsx
+++ coello-one/app/components/navbar-components/NavbarSearch/index.tsx
@@ -1,48 +1,31 @@
 "use client";
 
-import Search from "antd/es/input/Search";
-import { ChangeEvent, MouseEvent, KeyboardEvent, JSX } from "react";
+import Search, { SearchProps } from "antd/es/input/Search";
+import { JSX } from "react";
 
 interface NavbarSearchProps {
   className: string;
   showSearch: () => void;
 }
 
-type HandleSearchTypes = {
-  value: string;
-  event?:
-    | ChangeEvent<HTMLInputElement>
-    | MouseEvent<HTMLElement>
-    | KeyboardEvent<HTMLInputElement>
-    | undefined;
-  info?: { source?: "input" | "clear" | undefined } | undefined;
-};
-
 export function NavbarSearch({
   className,
   showSearch,
 }: NavbarSearchProps): JSX.Element {
-  const handleSearch = (
-    value: string,
-    event?: HandleSearchTypes["event"],
-    info?: HandleSearchTypes["info"]
-  ): void => {
-    console.log("info:", info?.source);
-    console.log("event:", event);
-    console.log(value);
-    if (info?.source === "clear") {
-      console.log("clear");
-    }
-  };
+  const handleSearch: SearchProps["onSearch"] = (value, _e, info) =>
+    console.log(info?.source, value);
+
   return (
     <Search
       placeholder="Let us help you find what you need"
       //   loading
-      className={`${className} w-4/5 ml-auto`}
+      className={`${className} w-11/12 ml-auto`}
       onSearch={handleSearch}
       allowClear
       onBlur={showSearch}
       id="navbar-search"
+      size="large"
+      // variant="borderless"
     />
   );
 }
