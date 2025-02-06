45485c360da57cc470bc139599f24089dc49d69e
dtoxvanilla1991 <dtoxvanilla@gmail.com>
2025-02-05 20:51:11 -0500

    fix: sidemenu ui logic and view

--- coello-one/app/components/navbar-components/NavbarSider/SideMenu.tsx
+++ coello-one/app/components/navbar-components/NavbarSider/SideMenu.tsx
@@ -1,54 +1,64 @@
 import { Menu } from "antd";
 import type { MenuProps } from "antd";
-import { RiseOutlined, GoldOutlined, PlusOutlined } from "@ant-design/icons";
 import { useRouter, usePathname } from "next/navigation";
 import { Key, ReactNode } from "react";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
+function createMenuTitle(
   label: ReactNode,
   key: Key,
-  icon?: ReactNode,
   children?: MenuItem[]
 ): MenuItem {
   return {
     key,
     label,
-    icon,
     children,
+    className: "uppercase font-medium",
+  } as MenuItem;
+}
+function createMenuOption(
+  label: ReactNode,
+  key: Key,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    label,
+    children,
+    className: "!text-gray-600 capitalize",
   } as MenuItem;
 }
 
 const items: MenuItem[] = [
-  getItem("Trending", "/trending", <RiseOutlined />, [
-    getItem("Best Sellers", "/collections/best-sellers/men"),
+  createMenuTitle("Trending", "/trending", [
+    createMenuOption("Best Sellers", "/collections/best-sellers/men"),
   ]),
-  getItem("Products", "/products", <GoldOutlined />, [
-    getItem("Passion model", "/products/passion-model"),
-    getItem("Power model", "/products/power-model"),
-    getItem("Pride model", "/products/pride-model"),
+  createMenuTitle("Products", "/products", [
+    createMenuOption("Passion model", "/products/passion-model"),
+    createMenuOption("Power model", "/products/power-model"),
+    createMenuOption("Pride model", "/products/pride-model"),
   ]),
   // this idea needs to be properly developed
-  //   getItem("Coello One Hub", "/hub", <UserOutlined />, [
-  //     getItem("Brandon's cut workout routine", "/hub/athlete/brandon-j-plan"),
-  //     getItem(
+  //   createMenuTitle("Coello One Hub", "/hub", <UserOutlined />, [
+  //     createMenuTitle("Brandon's cut workout routine", "/hub/athlete/brandon-j-plan"),
+  //     createMenuTitle(
   //       "How to scalp cannonball shoulders?",
   //       "/hub/media/how-to-scalp-cannonball-shoulders"
   //     ),
-  //     getItem("Julia's lean look diet", "/hub/athlete/julia-c-plan"),
-  //     getItem(
+  //     createMenuTitle("Julia's lean look diet", "/hub/athlete/julia-c-plan"),
+  //     createMenuTitle(
   //       "Want to see diet work fast? Fast.",
   //       "/hub/media/want-to-see-diet-work-fast"
   //     ),
-  //     getItem("Laura's healthy tips for busy pros", "/hub/athlete/laura-g-plan"),
-  //     getItem(
+  //     createMenuTitle("Laura's healthy tips for busy pros", "/hub/athlete/laura-g-plan"),
+  //     createMenuTitle(
   //       "How to properly protect your tattoo",
   //       "/hub/media/how-to-properly-protect-your-tattoo"
   //     ),
   //   ]),
-  getItem("Accessories", "/accessories", <PlusOutlined />, [
-    getItem("Resistance Bands", "/accessories/resistance-bands"),
+  createMenuTitle("Accessories", "/accessories", [
+    createMenuOption("Resistance Bands", "/accessories/resistance-bands"),
   ]),
 ];
 
