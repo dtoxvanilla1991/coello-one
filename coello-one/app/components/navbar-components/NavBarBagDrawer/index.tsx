c76e1cd83254c1754ca3bb5e69db366643ec4be4
dtoxvanilla1991 <dtoxvanilla@gmail.com>
2025-02-06 22:40:21 -0500

    feat: created NavbarBagDrawer component

---
+++ coello-one/app/components/navbar-components/NavBarBagDrawer/index.tsx
@@ -0,0 +1,54 @@
+import EmptyBagIcon from "@public/icons/EmptyBag";
+import { Button, Drawer, Empty, Space, Typography } from "antd";
+
+const { Text } = Typography;
+
+interface NavBarBagDrawerProps {
+  showBag: boolean;
+  handleShowBag: () => void;
+}
+
+const NavBarBagDrawer: React.FC<NavBarBagDrawerProps> = ({
+  showBag,
+  handleShowBag,
+}) => {
+  const onClick = () => {
+    console.log("View Bag");
+  };
+
+  return (
+    <Drawer
+      title="YOUR BAG"
+      placement={"bottom"}
+      closable
+      // leaving this until we have products to display
+      //   footer={
+      //     <Button onClick={onClick} className="uppercase">
+      //       View Bag
+      //     </Button>
+      //   }
+      onClose={handleShowBag}
+      open={showBag}>
+      <Empty
+        description="Your bag is empty"
+        className="uppercase"
+        image={<EmptyBagIcon />}
+        styles={{ image: { marginBottom: 0 } }}>
+        <Space direction="vertical">
+          <Button type="primary" className="w-full">
+            <Text className="uppercase !text-white" strong>
+              Shop men
+            </Text>
+          </Button>
+          <Button type="primary">
+            <Text className="uppercase !text-white" strong>
+              Shop women
+            </Text>
+          </Button>
+        </Space>
+      </Empty>
+    </Drawer>
+  );
+};
+
+export default NavBarBagDrawer;
